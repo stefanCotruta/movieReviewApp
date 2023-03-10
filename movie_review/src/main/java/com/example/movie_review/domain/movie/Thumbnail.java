0a1c47ee9babdb2aeb6c4e14fd7a0d665c964fb2
Stefan Cotruta <stefan@Stefans-MacBook-Air.local>
2023-03-10 19:35:28 +0200

    Implemented file upload feature

---
+++ movie_review/src/main/java/com/example/movie_review/domain/movie/Thumbnail.java
@@ -0,0 +1,18 @@
+package com.example.movie_review.domain.movie;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Thumbnail {
+    private String filename;
+    private String fileType;
+    private String fileSize;
+    private byte[] file;
+
+public Thumbnail(){};
+
+
+}
